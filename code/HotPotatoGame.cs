097b56f1edddee7d4b2e619c9a1bf09e33b46be5
Erin <28122025+AWildErin@users.noreply.github.com>
2021-05-12 19:34:35 +0100

    Add basic game code

---
+++ code/HotPotatoGame.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox;
+using System;
+
+namespace HotPotato
+{
+	[Library("hotpotato")]
+	partial class HotPotatoGame : Game
+	{
+		public HotPotatoGame()
+		{
+			if ( IsServer )
+				new HotPotatoHud();
+		}
+
+		public override Player CreatePlayer()
+		{
+			return new HotPotatoPlayer();
+		}
+	}
+}
---
+++ code/HotPotatoHud.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace HotPotato
+{
+	partial class HotPotatoHud : Hud
+	{
+		public HotPotatoHud()
+		{
+			if ( !IsClient )
+				return;
+
+			RootPanel.AddChild<NameTags>();
+			RootPanel.AddChild<CrosshairCanvas>();
+			RootPanel.AddChild<ChatBox>();
+			RootPanel.AddChild<KillFeed>();
+		}
+	}
+}
