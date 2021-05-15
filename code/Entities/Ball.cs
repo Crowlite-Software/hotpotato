d864af439e62c4273aa10b9479edde6862027ddb
Aspect <xenochris12@gmail.com>
2021-05-15 18:33:20 +0300

    Improve potato collision.
    StartTouch on the potato is more accurate than StartTouch on the player, apparently.

--- code/Entities/Ball.cs
+++ code/Entities/Ball.cs
@@ -5,7 +5,7 @@ using HotPotato;
 
 namespace HotPotato.Entities
 {
-    [Library( "ent_ball", Title = "Bouncy Ball")]
+    [Library("ent_ball", Title = "Bouncy Ball")]
     partial class BallEntity : Prop
     {
         public PickupTrigger PickupTrigger { get; protected set; }
@@ -14,24 +14,36 @@ namespace HotPotato.Entities
         {
             base.Spawn();
 
-            SetModel( "models/potato/ball.vmdl" );
-            SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
+            SetModel("models/potato/ball.vmdl");
+            SetupPhysicsFromModel(PhysicsMotionType.Dynamic, false);
             WorldScale = 1;
             RenderColor = Color.Random.ToColor32();
 
             PickupTrigger = new PickupTrigger
-		    {
-		    	Parent = this,
-		    	WorldPos = WorldPos,
-		    	EnableTouch = true
-		    };
+            {
+                Parent = this,
+                WorldPos = WorldPos,
+                EnableTouch = true
+            };
         }
 
-        protected override void OnPhysicsCollision( CollisionEventData eventData )
+        protected override void OnPhysicsCollision(CollisionEventData eventData)
         {
             var speed = eventData.PreVelocity.Length;
-            var direction = Vector3.Reflect( eventData.PreVelocity.Normal, eventData.Normal.Normal ).Normal;
+            var direction = Vector3.Reflect(eventData.PreVelocity.Normal, eventData.Normal.Normal).Normal;
             Velocity = direction * (speed - 50f);
+        }
+
+        // Pick the potato back up.
+        public override void StartTouch(Entity other)
+        {
+            if (IsClient) return;
+
+            if (other is HotPotato.HotPotatoPlayer && !HotPotato.HotPotatoPlayer.PlayerHasPotato) // Don't pick up it up if we already have one.
+            {
+                HotPotato.HotPotatoPlayer.PlayerHasPotato = true; // We picked the potato up.
+                Delete();
+            }
         }
     }
 }
--- code/HotPotatoPlayer.cs
+++ code/HotPotatoPlayer.cs
@@ -5,123 +5,111 @@ using System;
 
 namespace HotPotato
 {
-	partial class HotPotatoPlayer : BasePlayer
-	{
-		private TimeSince timeSinceDropped;
-		private TimeSince timeSinceJumpReleased;
+    partial class HotPotatoPlayer : BasePlayer
+    {
+        private TimeSince timeSinceDropped;
+        private TimeSince timeSinceJumpReleased;
 
-		private DamageInfo lastDamage;
+        private DamageInfo lastDamage;
 
-		private bool PlayerHasPotato;
+        public static bool PlayerHasPotato;
 
-		public override void Respawn()
-		{
-			SetModel( "models/citizen/citizen.vmdl" );
+        public override void Respawn()
+        {
+            SetModel("models/citizen/citizen.vmdl");
 
-			//
-			// Use WalkController for movement (you can make your own PlayerController for 100% control)
-			//
-			Controller = new WalkController();
+            //
+            // Use WalkController for movement (you can make your own PlayerController for 100% control)
+            //
+            Controller = new WalkController();
 
-			//
-			// Use StandardPlayerAnimator  (you can make your own PlayerAnimator for 100% control)
-			//
-			Animator = new StandardPlayerAnimator();
+            //
+            // Use StandardPlayerAnimator  (you can make your own PlayerAnimator for 100% control)
+            //
+            Animator = new StandardPlayerAnimator();
 
-			//
-			// Use FirstPersonCamera (you can make your own Camera for 100% control)
-			//
-			Camera = new FirstPersonCamera();
+            //
+            // Use FirstPersonCamera (you can make your own Camera for 100% control)
+            //
+            Camera = new FirstPersonCamera();
 
-			EnableAllCollisions = true;
-			EnableDrawing = true;
-			EnableHideInFirstPerson = true;
-			EnableShadowInFirstPerson = true;
-			PlayerHasPotato = true;
+            EnableAllCollisions = true;
+            EnableDrawing = true;
+            EnableHideInFirstPerson = true;
+            EnableShadowInFirstPerson = true;
+            PlayerHasPotato = true;
 
-			base.Respawn();
-		}
+            base.Respawn();
+        }
 
-		/// <summary>
-		/// Called every tick, clientside and serverside.
-		/// </summary>
-		protected override void Tick()
-		{
-			base.Tick();
+        /// <summary>
+        /// Called every tick, clientside and serverside.
+        /// </summary>
+        protected override void Tick()
+        {
+            base.Tick();
 
-			if ( Input.Pressed( InputButton.View ) )
-			{
-				if ( Camera is not FirstPersonCamera )
-				{
-					Camera = new FirstPersonCamera();
-				}
-				else
-				{
-					Camera = new ThirdPersonCamera();
-				}
-			}
+            if (Input.Pressed(InputButton.View))
+            {
+                if (Camera is not FirstPersonCamera)
+                {
+                    Camera = new FirstPersonCamera();
+                }
+                else
+                {
+                    Camera = new ThirdPersonCamera();
+                }
+            }
 
-			//
-			// Shoot a ball when Mouse1 is pressed.
-			//
-			if ( IsServer && Input.Pressed( InputButton.Attack1 ) && PlayerHasPotato )
-			{
-				var potato = new BallEntity();
-				potato.WorldPos = EyePos + EyeRot.Forward * 50;
-				potato.Velocity = EyeRot.Forward * 1000;
-				PlayerHasPotato = false; // We threw the potato, so we don't have it anymore.
-			}
-		}
+            //
+            // Shoot a ball when Mouse1 is pressed.
+            //
+            if (IsServer && Input.Pressed(InputButton.Attack1) && PlayerHasPotato)
+            {
+                var potato = new BallEntity();
+                potato.WorldPos = EyePos + EyeRot.Forward * 50;
+                potato.Velocity = EyeRot.Forward * 1000;
+                PlayerHasPotato = false; // We threw the potato, so we don't have it anymore.
+            }
+        }
 
-		// Pick the potato back up.
-		public override void StartTouch( Entity other )
-		{
-			if ( IsClient ) return;
+        #region Death Code
+        public override void OnKilled()
+        {
+            base.OnKilled();
 
-			if ( other is BallEntity && !PlayerHasPotato ) // Don't pick up it up if we already have one.
-			{
-				PlayerHasPotato = true; // We picked the potato up.
-				other.Delete();
-			}
-		}
+            BecomeRagdollOnClient(Velocity, lastDamage.Flags, lastDamage.Position, lastDamage.Force, GetHitboxBone(lastDamage.HitboxIndex));
+            Camera = new RagdollCamera();
 
-#region Death Code
-		public override void OnKilled()
-		{
-			base.OnKilled();
+            Controller = null;
 
-			BecomeRagdollOnClient( Velocity, lastDamage.Flags, lastDamage.Position, lastDamage.Force, GetHitboxBone( lastDamage.HitboxIndex ) );
-			Camera = new RagdollCamera();
+            EnableAllCollisions = false;
+            EnableDrawing = false;
+        }
 
-			Controller = null;
+        public override void TakeDamage(DamageInfo info)
+        {
+            lastDamage = info;
 
-			EnableAllCollisions = false;
-			EnableDrawing = false;
-		}
+            TookDamage(lastDamage.Flags, lastDamage.Position, lastDamage.Force);
 
-		public override void TakeDamage( DamageInfo info )
-		{
-			lastDamage = info;
+            base.TakeDamage(info);
+        }
 
-			TookDamage( lastDamage.Flags, lastDamage.Position, lastDamage.Force );
+        [ClientRpc]
+        public void TookDamage(DamageFlags damageFlags, Vector3 forcePos, Vector3 force)
+        {
+        }
+        #endregion
 
-			base.TakeDamage( info );
-		}
+        // Remove before release
+        public override bool HasPermission(string mode)
+        {
+            if (mode == "noclip") return true;
+            if (mode == "devcam") return true;
+            if (mode == "suicide") return true;
 
-		[ClientRpc]
-		public void TookDamage( DamageFlags damageFlags, Vector3 forcePos, Vector3 force )
-		{
-		}
-#endregion
-
-		// Remove before release
-		public override bool HasPermission( string mode )
-		{
-			if ( mode == "noclip" ) return true;
-			if ( mode == "devcam" ) return true;
-			if ( mode == "suicide" ) return true;
-
-			return base.HasPermission( mode );
-		}
-	}
+            return base.HasPermission(mode);
+        }
+    }
 }
